a82997a6a2ac0be42b672d4e91939d91c8d302fc
higorx1xd <96533233+higorx1xd@users.noreply.github.com>
2021-12-22 12:21:05 -0300

    Initial commit

---
+++ src/HelloWorld/Main.java
@@ -0,0 +1,13 @@
+package HelloWorld;
+
+import java.sql.SQLOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    public static void main(String[] args){
+        System.out.println(("Testing commit");
+    }
+}
